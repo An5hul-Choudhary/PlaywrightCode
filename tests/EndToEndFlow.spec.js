5bc4f51d60180f78267fe06b9ba1c039ae4ce174
Anshul Choudhary <anshulp115@gmail.com>
2024-04-13 15:04:46 +0530

    PressSequentially

--- tests/EndToEndFlow.spec.js
+++ tests/EndToEndFlow.spec.js
@@ -3,12 +3,12 @@ const { test, expect } = require('@playwright/test');
   
 test('@Client App login', async ({ page }) => {
    //js file- Login js, DashboardPage
-   const email = "anshika@gmail.com";
-   const productName = 'zara coat 3';
+   const email = "anshika222@gmail.com";
+   const productName = 'ADIDAS ORIGINAL';
    const products = page.locator(".card-body");
    await page.goto("https://rahulshettyacademy.com/client");
    await page.locator("#userEmail").fill(email);
-   await page.locator("#userPassword").type("Iamking@000");
+   await page.locator("#userPassword").fill("Iamking@000");
    await page.locator("[value='Login']").click();
    await page.waitForLoadState('networkidle');
    const titles = await page.locator(".card-body b").allTextContents();
@@ -26,11 +26,11 @@ test('@Client App login', async ({ page }) => {
    //await page.pause();
  
    await page.locator("div li").first().waitFor();
-   const bool = await page.locator("h3:has-text('zara coat 3')").isVisible();
+   const bool = await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible();
    expect(bool).toBeTruthy();
    await page.locator("text=Checkout").click();
  
-   await page.locator("[placeholder*='Country']").type("ind");
+   await page.locator("[placeholder*='Country']").pressSequentially("ind");
  
    const dropdown = page.locator(".ta-results");
    await dropdown.waitFor();
@@ -48,6 +48,22 @@ test('@Client App login', async ({ page }) => {
    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
    console.log(orderId);
+
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+ 
+ 
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
+ 
 
 });
  
