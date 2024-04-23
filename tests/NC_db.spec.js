dd86a43257dd7ac6467b97b86ac61151484bcf50
Anshul Choudhary <161596930+An5hul-Choudhary@users.noreply.github.com>
2024-04-23 17:03:45 +0530

    NC_db

---
+++ tests/NC_db.spec.js
@@ -0,0 +1,19 @@
+const {test} = require('@playwright/test')
+
+test("DB query execution test", async ({page}) =>{
+
+    // await page.goto("http://telus1005.netcracker.com:6910/tools/db150.jsp");
+    // await page.locator("#user").fill("sysadm");
+    // await page.locator("#pass").fill("netcracker");
+    // await page.locator(".buttonInner").click();
+    // await page.locator("#confirm").click();
+    // await page.locator("#id_sql").fill("select * from cloud_catalog_synchronization;");
+    // await page.locator("#submit_exec").click();
+    // // await page.goto("http://telus1005.netcracker.com:6910/tools/db150.jsp");
+    // await page.pause();
+
+    
+
+
+    
+})
